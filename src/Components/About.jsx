f5909ad67d19c4687dc26d403abff22197b5a949
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-03-23 11:45:11 +0530

    Merged remote changes

---
+++ src/Components/About.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div className="flex justify-between w-screen min-h-screen overflow-hidden">
+        <div className="w-screen md:w-screen h-screen bg-gradient-to-br from-red-600 via-red-500 to-red-700 animate-gradient-move flex justify-center items-center px-4 overflow-hidden">
+        </div>
+      </div>
+    );
+  }
