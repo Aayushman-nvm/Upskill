764e79734021cd2ad03d9a72dd711702a8671b43
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-04-20 13:35:26 +0530

    Removed depresiating oprions

--- backend/index.js
+++ backend/index.js
@@ -39,10 +39,7 @@ app.use(passport.session());
 app.use("/auth", authRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error(err));
 
